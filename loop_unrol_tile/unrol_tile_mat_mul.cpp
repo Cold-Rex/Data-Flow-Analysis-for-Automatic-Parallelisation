6d155fa2fb3e1577106b752a2548a3acc1369345
111701006@smail.iitpkd.ac.in <111701006@smail.iitpkd.ac.in>
2021-04-24 17:33:24 +0530

    unroll file changed

--- loop_unrol_tile/unrol_tile_mat_mul.cpp
+++ loop_unrol_tile/unrol_tile_mat_mul.cpp
@@ -3,38 +3,45 @@
 #include <omp.h>
 using namespace std;
 using namespace std::chrono;
-double arr1[500][500],arr2[500][500],res[500][500];
+const int p = 500 ;
+const int q = 500 ;
+const int r = 500 ;
+double arr1[p][q],arr2[q][r],res[p][r];
 #define MIN(x,y) (((x)>(y)) ? (y):(x))
 int main()
 {
     
     #pragma omp parallel for collapse(2)
-    for(int i=0;i<500;i++)
+    for(int i=0;i<p;i++)
     {
-        for(int j=0;j<500;j++)
+        for(int j=0;j<q;j++)
         {
             arr1[i][j]=0.5;
-            arr2[i][j]=0.5;
-            res[i][j]=0.0;
+            for(int k=0;k<r;k++)
+            {
+                arr2[j][k]=0.5;
+                res[i][k]=0.0;
+            }
         }
     }
-    int B=50; //tile size
+ 
+    int B=30; //tile size
     int N=500;
     auto start = high_resolution_clock::now(); 
     #pragma omp parallel for collapse(1)
-    for(int x=0;x<N;x=x+B)
+    for(int x=0;x<p;x=x+B)
     {
-        for(int y=0;y<N;y=y+B)
+        for(int y=0;y<q;y=y+B)
         {
-            for(int z=0;z<N;z=z+B)
+            for(int z=0;z<r;z=z+B)
             {
-                for(int i=x;i<MIN(N,(x+B));i++)
+                for(int i=x;i<MIN(p,(x+B));i++)
                 {
-                    for(int j=y;j<MIN(N,(y+B));j++)
+                    for(int j=y;j<MIN(q,(y+B));j++)
                     {
-                        for(int k=z;k<MIN(N,(z+B));k++)
+                        for(int k=z;k<MIN(r,(z+B));k++)
                         {
-                            res[i][j]=res[i][j]+((arr1[i][k])*(arr2[k][j]));
+                            res[i][k]=res[i][k]+((arr1[i][j])*(arr2[j][k]));
                             
                         }
                     }
